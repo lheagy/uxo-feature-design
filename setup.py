bae9ac5cc11bae43d952254639f2beaf5bff4de5
Lindsey Heagy <lindseyheagy@gmail.com>
2022-05-17 12:09:02 -0700

    add environment file, setup

---
+++ setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+"""polarizability_model
+
+Simulate and invert
+"""
+
+from distutils.core import setup
+from setuptools import find_packages
+
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python",
+    "Topic :: Scientific/Engineering",
+    "Topic :: Scientific/Engineering :: Mathematics",
+    "Topic :: Scientific/Engineering :: Physics",
+    "Operating System :: Microsoft :: Windows",
+    "Operating System :: POSIX",
+    "Operating System :: Unix",
+    "Operating System :: MacOS",
+    "Natural Language :: English",
+]
+
+with open("README.md") as f:
+    LONG_DESCRIPTION = "".join(f.readlines())
+
+setup(
+    name="SimPEG",
+    version="0.17.0",
+    packages=find_packages(exclude=["tests*", "notebooks*"]),
+    install_requires=[
+        "numpy>=1.7",
+        "scipy>=1.0.0",
+        "SimPEG"
+    ],
+    author="Lindsey Heagy",
+    author_email="lheagy@eoas.ubc.ca",
+    description="polarizability_model",
+    long_description=LONG_DESCRIPTION,
+    license="MIT",
+    keywords="unexploded ordnance, magnetics, electromagnetics",
+    url="https://github.com/lheagy/uxo-feature-design",
+    download_url="https://github.com/lheagy/uxo-feature-design",
+    classifiers=CLASSIFIERS,
+    platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
+    use_2to3=False,
+)
